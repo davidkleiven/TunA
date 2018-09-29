d6d2492e2f33993d7959231181d71ebecba5d5ae
David Kleiven <davidkleiven446@gmail.com>
2018-09-29 11:29:25 +0200

    Exponential low pass filter

---
+++ app/src/main/java/com/github/tuna/ExponentialFilter.java
@@ -0,0 +1,24 @@
+package com.github.tuna;
+
+public class ExponentialFilter extends Object{
+  private double damping = 0.01;
+  private int num_samples = 0;
+  private double signal;
+
+  /** Set a new damping factor */
+  public void setDamping(double decay){damping = Math.exp(-decay);};
+
+  /** Add a new sample to the signal */
+  public void addSample(double value){
+    num_samples += 1;
+    signal = signal*damping + value;
+  }
+
+  private double normalization(){
+    return (1.0 - Math.pow(damping, num_samples))/(1.0 - damping);
+  }
+
+  public double get(){
+    return signal/normalization();
+  }
+}

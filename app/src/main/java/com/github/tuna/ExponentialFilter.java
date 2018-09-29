1489a6ea7d5adbf568a51c7c46b72c369ffa3b67
David Kleiven <davidkleiven446@gmail.com>
2018-09-29 11:30:06 +0200

    Added reset method

--- app/src/main/java/com/github/tuna/ExponentialFilter.java
+++ app/src/main/java/com/github/tuna/ExponentialFilter.java
@@ -3,8 +3,13 @@ package com.github.tuna;
 public class ExponentialFilter extends Object{
   private double damping = 0.01;
   private int num_samples = 0;
-  private double signal;
+  private double signal = 0.0;
 
+  public void reset(){
+    num_samples = 0;
+    signal = 0.0;
+  }
+  
   /** Set a new damping factor */
   public void setDamping(double decay){damping = Math.exp(-decay);};
 
